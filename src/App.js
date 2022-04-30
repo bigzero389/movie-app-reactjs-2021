cfeca9f0279daf9a41bf6a66022c6688a1fa8cd7
bigzero <garack@gmail.com>
2022-04-30 16:12:50 +0900

    6-0 finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button";
-import styled from "./App.module.css"
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!!!!</h1>
-      <Button text="bigzero button" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
