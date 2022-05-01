2333928bbcd292b7ef9ed88d6c5bd81bd6595e13
bigzero <garack@gmail.com>
2022-05-01 14:11:54 +0900

    7-10 finished, all done

--- movie-app-reactjs/src/components/Movie.js
+++ movie-app-reactjs/src/components/Movie.js
@@ -1,19 +1,26 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
 function Movie ({movie}) {
   return (
-  <div>
-    <h2><Link to={`/movie/${movie.id}`}>Title: {movie.title}</Link></h2>
-    <img src={movie.medium_cover_image} alt={movie.title} />
-    <p>Summary: {movie.summary}</p>
-    <ul>Genres: {movie.genres.map( (g, i) => <li key={i}>{g}</li>)}</ul>
+  <div className={styles.movie}>
+    <img className={styles.movie__img} src={movie.medium_cover_image} alt={movie.title} />
+    <div>
+      <h2 className={styles.movie__title}><Link to={`/movie/${movie.id}`}>{movie.title}</Link></h2>
+      <h3 className={styles.movie__year}>{movie.year}</h3>
+      <p>
+        {movie.summary.length > 235 ? 
+          `${movie.summary.slice(0, 235)}...` :
+          movie.summary}
+      </p>
+      <ul className={styles.movie__genres}>{movie.genres.map( (g, i) => <li key={i}>{g}</li>)}</ul>
+    </div>
   </div>
   );
 }
 
 Movie.propTypes = {
-  // id: PropTypes.number.isRequired,
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     medium_cover_image: PropTypes.string.isRequired,
--- movie-app-reactjs/src/routes/Home.js
+++ movie-app-reactjs/src/routes/Home.js
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home () {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async() => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year&order_by=desc`)
-    ).json();
+    const json = await ( await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year&order_by=desc`)).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
@@ -17,9 +16,12 @@ function Home () {
   } , [] );
 
   return (
-    <div>
-      { loading ? <h1>Loading...</h1> : 
-        <div>
+    <div className={styles.container}>
+      { loading ? 
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div> :
+        <div className={styles.movies}>
           { movies.map( (movie) => ( 
            <Movie key={movie.id} movie={movie} /> 
           ) ) }
