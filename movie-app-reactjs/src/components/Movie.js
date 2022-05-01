f83dfe8b3b692ccc3163dbaaa6aefcf0070f01ac
bigzero <garack@gmail.com>
2022-05-01 12:55:22 +0900

    7-6 finished

--- movie-app-reactjs/src/components/Movie.js
+++ movie-app-reactjs/src/components/Movie.js
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie ({id, movie}) {
+function Movie ({movie}) {
   return (
-  <div key={id}>
-    <h2><Link to="/movie">Title: {movie.title}</Link></h2>
+  <div>
+    <h2><Link to={`/movie/${movie.id}`}>Title: {movie.title}</Link></h2>
     <img src={movie.medium_cover_image} alt={movie.title} />
     <p>Summary: {movie.summary}</p>
     <ul>Genres: {movie.genres.map( (g, i) => <li key={i}>{g}</li>)}</ul>
@@ -13,6 +13,7 @@ function Movie ({id, movie}) {
 }
 
 Movie.propTypes = {
+  // id: PropTypes.number.isRequired,
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     medium_cover_image: PropTypes.string.isRequired,
--- movie-app-reactjs/src/routes/Detail.js
+++ movie-app-reactjs/src/routes/Detail.js
@@ -1,5 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 function Detail() {
-  return <h1>Detail</h1>
+  const {id} = useParams(); // javascript 값을 jsx 에서 사용하려면 중괄호로 감싸주어야 한다.
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async () => {
+    const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+
+  useEffect( () => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      { loading ?  <h2>Loading...</h2> :
+      <div> 
+        <h2>Title: {movie.title}</h2>
+        <img src={movie.medium_cover_image} alt={movie.title} />
+        <p>Summary: {movie.summary}</p>
+        <ul>Genres: {movie.genres.map( (g, i) => <li key={i}>{g}</li>)}</ul>
+      </div>
+      }
+    </div>
+  );
 }
 
 export default Detail;
